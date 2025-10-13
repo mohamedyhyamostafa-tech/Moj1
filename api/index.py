4179e77df6a91973fc305b6424e59b90fccfeeea
mohamedyhyamostafa-tech <mohamedyhyamostafa@gmail.com>
2025-10-13 20:36:44 +0300

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="../static", static_url_path="/static")
+
+@app.route('/')
+def home():
+    return send_from_directory("../", "index.html")
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    return send_from_directory("../", path)
+
+handler = app  # مهم لـ Vercel
