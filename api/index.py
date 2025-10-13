693928c76342f7c69b9b912167ceb71c6bd4be69
mohamedyhyamostafa-tech <mohamedyhyamostafa@gmail.com>
2025-10-13 21:10:12 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, render_template_string
+from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder="../static", template_folder="../")
 
@@ -9,13 +9,6 @@ def home():
 @app.route("/static/<path:path>")
 def static_files(path):
     return send_from_directory("../static", path)
-
-@app.route("/api/score/<int:score>")
-def score(score):
-    if score >= 1000:
-        return {"status": "win", "message": "Congratulations! You saved the galaxy!"}
-    else:
-        return {"status": "keep going", "message": f"Your score: {score}"}
 
 if __name__ == "__main__":
     app.run(debug=True)
